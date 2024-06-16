cea0d2dc1d5509016303c66ed3849c56eeeaf6c3
pamquale <pamquale@gmail.com>
2024-06-16 18:19:51 +0300

    modified guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,27 @@
 use std::io::{self, Write};
+use std::cmp::Ordering;
 use rand::Rng;
 
-
 fn main() {
-    println!("Hello, world! It's a guessing game!");
+    println!("It's a guessing game!");
 
+    print!("Input the number between 1 and 5: ");
     let mut guess = String::new();
-    print!("Enter a number between 1 and 5: ");
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut guess).expect("Failed to read the line.");
 
     let guess: u32 = match guess.trim().parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Input the valid number");
+            println!{"Enter the valid number."};
             return;
         }
     };
     let number = rand::thread_rng().gen_range(1..5);
-    if guess == number {
-        println!("You guessed, it's {}!", number);
-    } else {
-        println!("You are wrong. It's {}", number);
+
+    match guess.cmp(&number) {
+        Ordering::Less => println!("Your number is to low! It was {}", number),
+        Ordering::Greater => println!("Your number is too big! It was {}", number),
+        Ordering::Equal => println!("You guessed! It was {}", number),
     }
 }
